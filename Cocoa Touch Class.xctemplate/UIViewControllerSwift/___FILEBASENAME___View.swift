277a34a34c73d450d5ec18607bd5a0dc13bae577
abagmut <abagmut@prophonix.com>
2015-08-27 19:00:27 +0300

    initial commit

---
+++ Cocoa Touch Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___View.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___View: UIView
+{
+
+    required init(coder aDecoder: NSCoder){ fatalError("init(coder:) has not been implemented") }
+    
+    init()
+    {
+        super.init(frame: CGRectZero)
+    }
+}
---
+++ Cocoa Touch Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ViewController: ___VARIABLE_cocoaTouchSubclass___
+{
+    override func loadView()
+    {
+        view = ___FILEBASENAMEASIDENTIFIER___View()
+    }
+}
